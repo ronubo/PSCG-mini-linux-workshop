7bca39378063e807ea27e1839eb0cf9f837b4638
Ron Munitz <ron@thepscg.com>
2020-03-04 22:06:19 +0200

    Updated paths for easier replacement at kernel tlv demo

--- envsetup.sh
+++ envsetup.sh
@@ -1,11 +1,12 @@
+BASEDIR=~/kertlv/demo
 export MACHINE=qemu
 export ARCH=x86_64
-export KV=linux-5.3-rc3
-export LABS=~/OSS-Aug-2019
-export KERNEL_SRC=~/OSS-Aug-2019/${KV}
-export KERNEL_OUT=~/OSS-Aug-2019/${KV}-${MACHINE}-${ARCH}-out
-export RAMDISK_WIP=~/OSS-Aug-2019/wip_ramdisk
-export RAMDISK_PACKED=~/OSS-Aug-2019/initramfs.gz
-export BUSYBOX_SRC=~/OSS-Aug-2019/busybox-1.30.1
-export JOBS=16
+export KV=linux-5.6-rc2
+export LABS=${BASEDIR}
+export KERNEL_SRC=${BASEDIR}/${KV}
+export KERNEL_OUT=${BASEDIR}/${KV}-${MACHINE}-${ARCH}-out
+export RAMDISK_WIP=${BASEDIR}/wip_ramdisk
+export RAMDISK_PACKED=${BASEDIR}/initramfs.gz
+export BUSYBOX_SRC=${BASEDIR}/busybox-1.30.1
+export JOBS=4
 
