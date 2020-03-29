099cb6a2c1c4b28cf9154d8422923fce61f50a05
Ron Munitz <ron@thepscg.com>
2020-03-29 21:18:51 +0300

    Updated busybox binary and modified init to focus on working on console
    The reason for this is that in thtis way it is easier to illustrate perfectly working version without mounting anything (i.e. no /proc /dev/ sys , ...), and show the differences and different fallbacks of the kernel init.
    
    Also added the "dontbuild" option to fetch_busybox_and_createt_initial_ramdisk.sh . Of course it is better to have different scripts, but I didn't want to have too many. when this argument is provided the script work as follows:
    - It does not fetch and unpack the busybox tarball.
    - If busybox is already built - it justs reinstall it.
    - Otherwise - it actutally does build it, which is the behavior we want - however it will only build for 64bit (FIXME)
    
    Note: since this repository is mostly used for training and the public talks are excerpts of the training, we will add and remove features according to the talk / interest / etc.  So sometimes it may seem to you as if "we made a progress, but take a step back". Keep calm, and follow the process :-)

--- fetch_busybox_and_create_initial_ramdisk.sh
+++ fetch_busybox_and_create_initial_ramdisk.sh
@@ -34,13 +34,18 @@ mount -t proc none /proc
 mount -t sysfs none /sys
 mount -t debugfs none /sys/kernel/debug
 
-echo -e "\n\033[0;32m The PSCG mini-linux\033[0m booted in \$(cut -d' ' -f1 /proc/uptime) seconds\n"
+echo -e "\n\033[0;32m The PSCG mini-linux\033[0m booted in $(cut -d' ' -f1 /proc/uptime) seconds\n"
 
 # Enable sysfs / dev initial enumeration
 mdev -s
 # Enable hotplugging - without this, the kernel would expect /sbin/hotplug. For this you need CONFIG_UEVENT_HELPER=y in the kernel, and you can live without it if CONFIG_DEVTMPFS=y and CONFIG_DEVTMPFS_MOUNT=y (and of course, CONFIG_SYSFS=y)
 # This line is used for other ramdisk generations with differnet illustration purposes, so don't worry if /proc/sys/kernel/hotplug doesn't exist, but /dev/mdev gets populated.
 echo /sbin/mdev >/proc/sys/kernel/hotplug
+
+# For our framebuffer example, show the splash screen. This is a userspace splash screen, and on the kernel graphics setup we cover the kernel boot logo. 
+# If the call fails (e.g. no framebuffer, or no splash binary) - no harm done, as it is userspace.
+# to avoid calling the splash screen, add "nosplash" to the kernel command line
+grep -q nosplash /proc/cmdline || /usr/bin/psplash
 
 # allow doing "bashrc" style stuff like aliases etc.
 # This can also be done by populating /etc/profile , however I am doing it in the following way for the sake of the example: 
@@ -51,7 +56,7 @@ export ENV=ronenv
 # get rid of annoying job control message - it is not really necessary.
 # also this is tailored for our exact scenario where we use the console as ttyS0 - be careful if you copy it to another platform, or in a graphical mode!
 # For a more elegant solution, use cttyhack; https://git.busybox.net/busybox/plain/shell/cttyhack.c?id=dcaed97
-exec setsid sh -c 'exec sh </dev/ttyS0 >/dev/ttyS0 2>&1'
+#exec setsid sh -c 'exec sh </dev/ttyS0 >/dev/ttyS0 2>&1'
 
 # Whatever is here will not run if you do not comment the previous line, because exec replaces the image.
 # Extra notes for demonstration: Comment the above (setsid line) if you run qemu with graphical mode and you want to use the console. Otherwise, you will think the console gets stuck - while it doesn't.
