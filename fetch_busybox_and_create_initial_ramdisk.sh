bc51b61cbad9ae655ef16f63e5668292efa357d1
Ron Munitz <ron@thepscg.com>
2024-08-17 20:22:09 +0200

    fetch_busybox_and_create_initial_ramdisk.sh: merged with the init of 2024/busybox-init-wip WITHOUT calling /sbin/init, and WITHOUT "EOF" for easier merging with current master
    Tested (with psplash after copying add_rootfs_extras.sh and rootfs-additions) by having:
    1. our-tiny-config -> vesa-fb-serial-ramdisk-sys-proc-elf-script-and-more.config  (this is not a part of this commit! we keep minimal, at least until I test and update vscode config)
    2. removing no-graphic, and instead adding append "init=/init  console=tty0 kgdboc.kgdboc=tty0 -vga=0x312"
       (can do console=ttyS0, and add -serial mon:stdio etc, . but this has the nice advantage of showing qemu monitor, and "sendkey ctrl+alt+f1" etc.)
    
    It is recommended to add add_rootfs_extras.sh and rootfs-additions/ and
    perhaps I will add them to this branch (and maybe also master later) as well.

--- fetch_busybox_and_create_initial_ramdisk.sh
+++ fetch_busybox_and_create_initial_ramdisk.sh
@@ -41,14 +41,22 @@ if [ $config = true ] ; then
 fi
 
 if [ $build = true ] ; then
-	make -C $BUSYBOX_VERSION CONFIG_PREFIX=$RAMDISK_WIP install -j$JOBS
+	# Note that this requires multilib (which will force you remove other distro installed cross-compilers), and i386 packages. 
+	# busybox 1.36.1 wants libcrypt which has no trivial i386 package in recent Ubuntu distros,
+	# so this is not recommended. better install an i386/i586/i686/... cross toolchain.
+	if [ "${ARCH}" = "i386" -a -z "$CROSS_COMPILE" -a "$(arch)" = "x86_64" ] ; then
+		CPPFLAGS=-m32 LDFLAGS=-m32 make -C $BUSYBOX_VERSION -j16 CONFIG_PREFIX=$RAMDISK_WIP install
+	else
+		make -C $BUSYBOX_VERSION CONFIG_PREFIX=$RAMDISK_WIP install -j$JOBS
+	fi
 fi
 
 # Create an initial directory structure
-mkdir -p wip_ramdisk/{lib,proc,sys,dev,mnt,xbin}
+mkdir -p $RAMDISK_WIP/{lib,proc,sys,dev,mnt,xbin,tmp}
+chmod +t  $RAMDISK_WIP/tmp
 
 # Populate init script
-cat > wip_ramdisk/init << EOF
+cat > $RAMDISK_WIP/init << EOF
 #!/bin/sh
 
 # If ramdisk doesn't come with premade empty proc sys dev folders - you can mkdir them here.
@@ -71,6 +79,42 @@ echo /sbin/mdev >/proc/sys/kernel/hotplug
 # You can also just populate it yourself from your running shell e.g. # ' echo "alias ll='ls -l' > ronenv , then execute 'sh' and you will see that you will the alias in the other shell
 export ENV=ronenv 
 
+[ -c /dev/dri/card0 ] && echo -e "\x1b[32mIdentified DRM device\x1b[0m"
+[ -c /dev/fb0 ] && echo -e "\x1b[32mIdentified Framebuffer device\x1b[0m"
+# Deliberately print "QEMU" if qemu is identified.
+if grep QEMU /sys/class/dmi/id/sys_vendor ; then
+	mkdir /mnt/host
+	# if it doesn't mount, no harm done. The purpose here is to be very brief, not accurate, and it is an addition made to explain some things in a talk
+	# The output in this case would be something like:
+	# 	9pnet_virtio: no channels available for device thepscg-mounts-you
+	# 	mount: mounting thepscg-mounts-you on /mnt/host failed: No such file or directory
+	mount -t 9p -o trans=virtio,version=9p2000.L  thepscg-mounts-you /mnt/host
+fi
+
+
+# You could communicate with psplash (which is either run from inittab or here, depending if /sbin/init is chosen, so we "start communicating" before we run it)
+# by doing something like the next line. You can also choose to avoid it.
+# The next line means the timeout 10, that I put if you run psplash from this init file, will not be met becuase we give it less time.
+if ! grep -q nosplash /proc/cmdline && [ -x /usr/bin/psplash ] ; then
+	[ -p /tmp/psplash_fifo ] || mkfifo /tmp/psplash_fifo
+
+	# We can also use /extra/progress_psplash.sh - it waits longer and displays some text.
+	# Anyway, do note that the current version has psplash built only for x86_64, so no point in bothering too much about it
+	# /extra/progress_splash.sh &
+	( for i in \$(seq 10 5 100) ; do echo -e "PROGRESS \$i\0" > /tmp/psplash_fifo ; sleep 0.1 ; done ; echo "QUIT" > /tmp/psplash_fifo) &
+fi
+
+
+# Comment out if you do not wish to do the logic in inittab
+# Since we show a splash screen in inittab, if you comment it out, we will "fallback" to showing the splash screen here
+# exec /sbin/init
+
+# For our framebuffer example, show the splash screen. This is a userspace splash screen, and on the kernel graphics setup we cover the kernel boot logo. 
+# If the call fails (e.g. no framebuffer, or no splash binary) - no harm done, as it is userspace.
+# to avoid calling the splash screen, add "nosplash" to the kernel command line
+grep -q nosplash /proc/cmdline || /usr/bin/psplash --timeout 10 &
+
+
 # get rid of annoying job control message - it is not really necessary.
 # also this is tailored for our exact scenario where we use the console as ttyS0 - be careful if you copy it to another platform, or in a graphical mode!
 # For a more elegant solution, use cttyhack; https://git.busybox.net/busybox/plain/shell/cttyhack.c?id=dcaed97
@@ -85,6 +129,6 @@ exec setsid cttyhack sh
 exec /bin/sh
 EOF
 
-chmod +x wip_ramdisk/init # Omit from full courses - explain briefly in short talks
+chmod +x $RAMDISK_WIP/init # Omit from full courses - explain briefly in short talks
 # Repackage the ramdisk
 exec ./repack_ramdisk.sh
